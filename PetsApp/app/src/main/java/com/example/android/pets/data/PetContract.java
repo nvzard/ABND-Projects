53152e89ea0284b8c7eb606c9b044f5fec44f067
Nitanshu Vashistha <nitanshu.vzard@gmail.com>
2018-10-13 19:34:22 +0530

    Pets App before Inventory Project

---
+++ PetsApp/app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,32 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+
+
+public final class PetContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private PetContract() {}
+
+    /**
+     * Inner class that defines constant values for the pets database table.
+     * Each entry in the table represents a single pet.
+     */
+    public static final class PetEntry implements BaseColumns {
+
+        /** Name of database table for pets */
+
+        public final static String TABLE_NAME = "pets";
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_PET_NAME ="name";
+        public final static String COLUMN_PET_BREED = "breed";
+        public final static String COLUMN_PET_GENDER = "gender";
+        public final static String COLUMN_PET_WEIGHT = "weight";
+
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+    }
+
+}
